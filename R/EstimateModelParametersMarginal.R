71468d9e20e9fcecdd58711a6138dc543ac0ab97
andreasottosanti <sottosanti@stat.unipd.it>
2022-02-28 13:24:10 +0100

    changes

--- R/EstimateModelParametersMarginal.R
+++ R/EstimateModelParametersMarginal.R
@@ -90,7 +90,7 @@ Estimate.Cocluster.Parameters.marginal.constraint.trace <- function(x,
         Tau[i] <- cur.tau
 
         logL[i] <- logL.Cocluster(x, Mu[i], Tau[i], traceDelta/p-Tau[i], Alpha[i], Beta[i], U, d)
-        if(round(logL[i] - logL[i-1], 3) < 0) stop("Decreasing loglikelihood within the M Step")
+        if(round(logL[i] - logL[i-1], 2) < 0) stop(cat("Decreasing loglikelihood within the M Step:",logL[i-1],"and",logL[i]))
         if((logL[i] - logL[i-1]) < threshold){
             converged <- T
             break}
